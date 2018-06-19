32a6d15335b12ffb608798470235dee7dbd52d82
Pooya Hatami <pooya@node-1w7jr9qrok5tbmfbapha9d96d.ipv6.telus.net>
2018-06-19 06:59:03 -0700

    ready!

--- IOS-Swift-SlideInMenuSW/ViewController.swift
+++ IOS-Swift-SlideInMenuSW/ViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menuBarItem: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        if revealViewController() != nil {
+            menuBarItem.target = self.revealViewController()
+            menuBarItem.action = #selector(SWRevealViewController().revealToggle(_:))
+            
+            self.view.addGestureRecognizer(revealViewController().panGestureRecognizer())
+        }
+        
     }
 
 
